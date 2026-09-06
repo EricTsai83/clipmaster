3f1ff918115010b1d7b14b397b5f555ac1c82417
Eric Tsai <eric492718@gmail.com>
2026-09-06 22:26:30 +0800

    Add API interface to Window and create preload module
    - Introduce a new `preload.ts` file defining a constant `api` and its type.
    - Extend the `Window` interface in `electron.d.ts` to include the `api` property for better type safety and integration with the preload script.

--- src/electron.d.ts
+++ src/electron.d.ts
@@ -3,3 +3,7 @@
 
 declare const MAIN_WINDOW_VITE_DEV_SERVER_URL: string | undefined;
 declare const MAIN_WINDOW_VITE_NAME: string;
+
+declare interface Window {
+  api: import("../src/preload").Api;
+}
